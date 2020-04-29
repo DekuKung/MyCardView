2c49c6ca92af017bdd67b784b8504113760c087a
DekuKung <07ghostharitkumsan@gmail.com>
2020-04-29 15:32:17 +0700

    CardView

---
+++ app/src/main/java/com/example/mycardview/CustomItem.java
@@ -0,0 +1,14 @@
+package com.example.mycardview;
+
+public class CustomItem {
+
+    public String title;
+    public int imgId;
+    public String content;
+
+    public CustomItem(String title, int imgId, String content){
+        this.title = title;
+        this.imgId = imgId;
+        this.content = content;
+    }
+}
